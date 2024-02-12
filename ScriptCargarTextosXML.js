e67a3bd25ca1edcb176e531ee6b11128ea184a07
FilipSMB <felix.insti@gmail.com>
2024-02-12 16:33:39 +0100

    Pagina de inicio

--- ScriptCargarTextosXML.js
+++ ScriptCargarTextosXML.js
@@ -21,10 +21,20 @@ window.addEventListener('DOMContentLoaded', function () {
             // Obtener los elementos de texto del XML
             var texto1XML = xmlDoc.querySelector('#texto-1');
             var texto2XML = xmlDoc.querySelector('#texto-2');
+            var texto3XML = xmlDoc.querySelector('#texto-3');
+
+            var titulo1XML = xmlDoc.querySelector('#titulo-1');
+            var titulo2XML = xmlDoc.querySelector('#titulo-2');
+            var titulo3XML = xmlDoc.querySelector('#titulo-3');
 
             // Insertar el contenido de los elementos XML en los elementos HTML
             texto1.textContent = texto1XML.textContent;
             texto2.textContent = texto2XML.textContent;
+            texto3.textContent = texto3XML.textContent;
+
+            titulo1.textContent = titulo1XML.textContent;
+            titulo2.textContent = titulo2XML.textContent;
+            titulo3.textContent = titulo3XML.textContent;
         }
     };
 
