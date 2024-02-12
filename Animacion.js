3436eafcd40f81b779e025cbf91d547e22b1b20b
FilipSMB <felix.insti@gmail.com>
2024-02-12 18:04:50 +0100

    Update Animacion.js

--- Animacion.js
+++ Animacion.js
@@ -1,29 +1,4 @@
-﻿//document.addEventListener("DOMContentLoaded", function () {
-//    var infoDivs = document.querySelectorAll('.info-div');
-
-//    // Función para animar la aparición de los divs de izquierda a derecha
-//    function animateDivs() {
-//        var delay = 0;
-//        infoDivs.forEach(function (div) {
-//            setTimeout(function () {
-//                div.style.display = 'block';
-//                div.style.opacity = '0';
-//                div.style.transform = 'translateX(-50px)';
-//                div.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
-//                setTimeout(function () {
-//                    div.style.opacity = '1';
-//                    div.style.transform = 'translateX(0)';
-//                }, 100);
-//            }, delay);
-//            delay += 500; // Ajusta el retraso entre cada div
-//        });
-//    }
-
-//    // Llama a la función de animación cuando se carga la página
-//    animateDivs();
-//});
-
-document.addEventListener("DOMContentLoaded", function () {
+﻿document.addEventListener("DOMContentLoaded", function () {
     var infoDivs = document.querySelectorAll('.info-div');
     var windowHeight = window.innerHeight;
 
@@ -37,22 +12,27 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    window.addEventListener('scroll', checkAndAnimate);
+    function animateDiv(div, delay) {
+        setTimeout(function () {
+            div.style.display = 'block';
+            div.style.opacity = '0';
+            div.style.transform = 'translateX(-50px)';
+            div.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+            checkAndAnimate();
+        }, delay);
+    }
 
     // Función para animar la aparición de los divs de izquierda a derecha
     function animateDivs() {
         var delay = 0;
         infoDivs.forEach(function (div) {
-            setTimeout(function () {
-                div.style.display = 'block';
-                div.style.opacity = '0';
-                div.style.transform = 'translateX(-50px)';
-                div.style.transition = 'opacity 0.5s ease, transform 0.5s ease';                
-            }, delay);
+            animateDiv(div, delay);
             delay += 500; // Ajusta el retraso entre cada div
         });
     }
 
     // Llama a la función de animación cuando se carga la página
     animateDivs();
+
+    window.addEventListener('scroll', checkAndAnimate); // Llama a checkAndAnimate() en cada evento de scroll
 });
