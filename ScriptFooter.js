4f4aba9eb42d4ae8e98e611e0c6521af5ea60b45
FilipSMB <felix.insti@gmail.com>
2024-02-19 16:29:51 +0100

    Fin de pagina

---
+++ ScriptFooter.js
@@ -0,0 +1,10 @@
+﻿window.addEventListener('scroll', function () {
+    var piePagina = document.getElementById('pie-pagina');
+    var alturaVentana = window.innerHeight;
+    var alturaTotal = document.body.offsetHeight;
+    var posicionActual = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (alturaTotal <= alturaVentana + posicionActual) {
+        piePagina.style.display = 'block';
+    } 
+});
